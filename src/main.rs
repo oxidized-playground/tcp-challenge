1ccf6f04b19da5c77601438532bf727cd0a16c94
Koen Braham <koen@braham.eu>
2025-04-23 17:18:03 +0000

    Update tcp challenge for 2025 edition

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use std::time;
 
+use serde::{Deserialize, Serialize};
+
 mod net;
 
 type ErrorMessage = String;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Message {
+    pub sender: String,
+    pub content: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), ErrorMessage> {
@@ -10,7 +18,7 @@ async fn main() -> Result<(), ErrorMessage> {
 }
 
 async fn main_loop() -> Result<(), ErrorMessage> {
-    let remote_address = "playground.foxlabs.nl:8080";
+    let remote_address = "rust.playgrounds.wolflabs.nl:8080";
     let mut client = net::client::Client::new(remote_address).await?;
 
     loop {
@@ -18,24 +26,24 @@ async fn main_loop() -> Result<(), ErrorMessage> {
         // message displayed on the screen?
         // Do use '.await?' to ensure the function gets executed.
 
-        let message = r#"{}"#;
+        let message = r#""#;
 
         // After sending, receive the reply from the server to print, it will tell you if you miss something in your message
         let reply = client.read().await?;
         println!("Received: {:?}", reply);
 
         // Extra challenge, can you send some struct?
-        // Modify the json_client in src/net/json_client.rs to serialize and deserialize messages
-        // let msg = net::json_client::Message {
-        //     sender: String::from("Your-Name"),
-        //     content: String::from("AAAA"),
-        // };
+        // Use serde to serialize and deserialize messages to and from the server
+        let msg = Message {
+            sender: String::from("Your-Name"),
+            content: String::from("AAAA"),
+        };
 
-        // client.write_message(&msg).await?;
-        // let reply = client.read_message().await?;
+        // client.send(&serialized_msg).await?;
+        // let reply = client.read().await?;
         // println!("Received: {:?}", reply);
 
-        let ten_seconds = time::Duration::from_secs(10);
+        let ten_seconds = time::Duration::from_millis(10);
         tokio::time::sleep(ten_seconds).await;
     }
 }
